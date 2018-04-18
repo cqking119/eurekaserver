a00baf7dcbf2c62933a98521b200f5f3488b9878
cqkings <894383469@qq.com>
2018-04-18 17:39:33 +0800

    注册中心提交

---
+++ src/main/java/springboot/SpringCloudEurekaApplication.java
@@ -0,0 +1,14 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
