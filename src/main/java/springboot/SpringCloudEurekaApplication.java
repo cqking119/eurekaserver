d0fab236c4608f7b2f4ce92013416d0447769813
cqkings <894383469@qq.com>
2018-04-18 18:17:28 +0800

    注册中心提交

--- src/main/java/springboot/SpringCloudEurekaApplication.java
+++ src/main/java/springboot/SpringCloudEurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
-@SpringBootApplication
+@SpringBootApplication //en
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
     public static void main(String[] args) {
